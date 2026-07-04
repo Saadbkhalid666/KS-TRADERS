beb45f87fac93a8ef64982b16ad149ecf591d305
Saad Bin Khalid <saadbkhalid666@gmail.com>
2026-07-04 17:32:46 +0500

    brand name

--- src/app/components/navbar.js
+++ src/app/components/navbar.js
@@ -1,16 +1,16 @@
+import { LucideShoppingCart } from "lucide-react"
 export const Navbar = () => {
 
     return <div className="container">
-        <nav className="flex">
-            <ul>
-                <h1>KS TRADERS</h1>
+        <nav className="flex flex-row justify-between  ">
+            <ul className="flex flex-row space-x-14 ml-2">
+                <h1 className="text-3xl text-[#e53935] ">KS TRADERS</h1>
                 <li>Home</li>
                 <li>Store</li>
                 <li>Support</li>
             </ul>
             <ul>
-                <li></li>
-                <li></li>
+                <li><LucideShoppingCart  size={24} /></li>
                 <li><button>Order Now</button></li>
             </ul>
         </nav>
