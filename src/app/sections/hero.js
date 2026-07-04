027f7b8f2c0573605ca035750e08401bda97ce1b
Saad Bin Khalid <saadbkhalid666@gmail.com>
2026-07-04 18:31:03 +0500

    hero.js

---
+++ src/app/sections/hero.js
@@ -0,0 +1,18 @@
+import PlasmaWave from "../backgrounds/plasmawave"
+
+export const Hero = ()=>{
+
+
+return 
+<PlasmaWave
+  colors={["#f5f5f5","#e53935"]}
+  speed1={0.05}
+  speed2={0.05}
+  focalLength={0.8}
+  bend1={1}
+  bend2={0.5}
+  dir2={1}
+  rotationDeg={0}
+/>
+
+}
