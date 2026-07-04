9f236b9c4aeabcc58a68e982f70432c703937999
Saad Bin Khalid <saadbkhalid666@gmail.com>
2026-07-04 17:24:15 +0500

    navbar.js

---
+++ src/app/components/navbar.js
@@ -0,0 +1,19 @@
+export const Navbar = () => {
+
+    return <div className="container">
+        <nav className="flex">
+            <ul>
+                <h1>KS TRADERS</h1>
+                <li>Home</li>
+                <li>Store</li>
+                <li>Support</li>
+            </ul>
+            <ul>
+                <li></li>
+                <li></li>
+                <li><button>Order Now</button></li>
+            </ul>
+        </nav>
+    </div>
+    
+}
