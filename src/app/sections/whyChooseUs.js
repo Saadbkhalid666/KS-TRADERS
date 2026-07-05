b9c5b32f284ffcb2da040919e545aace9ae7acbf
Saad Bin Khalid <saadbkhalid666@gmail.com>
2026-07-05 18:05:53 +0500

    whychooseus.js

--- src/app/sections/whyChooseUs.js
+++ src/app/sections/whyChooseUs.js
@@ -35,7 +35,7 @@ export const WhyChoseUs = () => {
   ];
 
   return (
-    <div className=" pt-20 pb-10 bg-[#e5e7eb] mx-auto ">
+    <div className=" pt-20 pb-10 bg-[#f8f8fa] mx-auto ">
         
 
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols3 lg:grid-cols-4 gap-4 ml-2 mr-2 ">
