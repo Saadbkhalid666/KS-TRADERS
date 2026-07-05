2ea3290fe7af08fe29f966bd491ec149efcbcf79
Saad Bin Khalid <saadbkhalid666@gmail.com>
2026-07-05 16:18:34 +0500

    hero.js completed

--- src/app/sections/hero.js
+++ src/app/sections/hero.js
@@ -1,42 +1,60 @@
 "use client";
 
 import DotField from "../components/background/dotField";
+import TargetCursor from "../components/cursor/cursor";
 
 export const Hero = () => {
   return (
-    <div className="bg-[#e5e7eb] h-screen w-full flex  justify-center ">
-      <div className="ml-6 mr-6">
-        <DotField
-          dotRadius={1.5}
-          dotSpacing={14}
-          bulgeStrength={67}
-          glowRadius={160}
-          sparkle={false}
-          waveAmplitude={0}
-          cursorRadius={500}
-          cursorForce={0.1}
-          bulgeOnly
-          gradientFrom="#A855F7"
-          gradientTo="#B497CF"
-          glowColor="#120F17"
-        >
-          <div className="contact-container flex flex-col  ">
-            <h1 className="text-6xl font-extrabold">
-              Premium Nestle Pure Life <br /> Water Delivered at <br />
-              <span>Wholesale Prices </span>
+    <div className="h-screen w-full bg-[#e5e7eb]">
+         <TargetCursor
+                spinDuration={2}
+                hideDefaultCursor
+                parallaxOn
+                hoverDuration={0.2}
+                cursorColor="#ffffff"
+                cursorColorOnTarget="#B497CF"
+              />
+      <DotField
+        dotRadius={1.5}
+        dotSpacing={14}
+        bulgeStrength={67}
+        glowRadius={160}
+        sparkle={false}
+        waveAmplitude={0}
+        cursorRadius={500}
+        cursorForce={0.1}
+        bulgeOnly
+        gradientFrom="#A855F7"
+        gradientTo="#B497CF"
+        glowColor="#120F17"
+      >
+        <div className="mx-auto flex h-screen max-w-7xl flex-col justify-center px-6">
+          <div className="max-w-3xl">
+            <h1 className="md:text-6xl  text-2xl sm:text-3xl font-extrabold leading-tight">
+              Premium Nestle Pure Life
+              Water Delivered at
+              <br />
+              <span className="text-[#e63539]">Wholesale Prices</span>
             </h1>
-            <p>
+
+            <p className="mt-6 max-w-2xl text-lg text-gray-700">
               Fuel your business with the world&apos;s most trusted hydration
               partner. Direct-to-warehouse delivery with transparent tiered
               pricing for procurement managers and high-volume outlets.
             </p>
+
+            <div className="mt-8 flex gap-4">
+              <button className="cursor-target rounded-lg bg-red-600 px-6 py-3 font-semibold text-white">
+                Shop Now
+              </button>
+
+              <button className="rounded-lg border border-gray-400 px-6 py-3 font-semibold">
+                Contact Sales
+              </button>
+            </div>
           </div>
-          <div>
-            <button>Shop Now</button>
-            <button>Contact Sales</button>
-          </div>
-        </DotField>
-      </div>
+        </div>
+      </DotField>
     </div>
   );
 };
