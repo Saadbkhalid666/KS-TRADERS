480ed45d0a3ec1f55ec536e2a04d80c54f00591e
Saad Bin Khalid <saadbkhalid666@gmail.com>
2026-07-05 17:00:11 +0500

    global.css

--- src/app/fonts/fonts.js
+++ src/app/fonts/fonts.js
@@ -1,5 +1,15 @@
 import localFont from "next/font/local"
+import { Inter, Sansation } from "next/font/google"
 
 export const melfira = localFont({
     src:"./melfira.ttf"
   })
+
+export const inter = Inter({
+  weight:"300",
+  display:"auto"
+})
+
+export const sansation = Sansation({
+  weight:"300"
+})
--- src/app/sections/whyChooseUs.js
+++ src/app/sections/whyChooseUs.js
@@ -1,5 +1,8 @@
+"use client"
 import { Banknote, ShieldCheck, Store, Truck } from "lucide-react";
-import { melfira } from "../fonts/fonts";
+import { melfira, sansation } from "../fonts/fonts";
+import DotField from "../components/background/dotField";
+import TargetCursor from "../components/cursor/cursor";
 
 export const WhyChoseUs = () => {
   const featuresCards = [
@@ -34,16 +37,29 @@ export const WhyChoseUs = () => {
   ];
 
   return (
-    <div className="mt-66 ">
-      <div className="flex flex-row ">
+    <div className=" mt-20 bg-[#e5e7eb] ">
+        <TargetCursor
+                spinDuration={2}
+                hideDefaultCursor
+                parallaxOn
+                hoverDuration={0.2}
+                cursorColor="#ffffff"
+                cursorColorOnTarget="#B497CF"
+              /> 
+
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols3 lg:grid-cols-4 gap-2 mx-auto ">
         {featuresCards.map((item, index) => {
-          return (
-            <div key={index} className="">
+            return (
+                <div key={index} className="bg-white px-10 py-10">
               <p className="text-[#e63539]  bg-red-300 w-max px-4 py-3  rounded-2xl  ">
                 {item.icon}
               </p>
-              <h1 className={`text-[#111111] text-2xl font-bold ${melfira.className} `}>{item.heading}</h1>
-              <p>{item.about}</p>
+              <h1
+                className={`text-[#111111] text-2xl font-bold ${melfira.className} `}
+                >
+                {item.heading}
+              </h1>
+              <p className={`${sansation.className}`}>{item.about}</p>
             </div>
           );
         })}
