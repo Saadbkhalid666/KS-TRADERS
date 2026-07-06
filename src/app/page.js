ea905b0e49679f8e0656b08d569a951152e226d9
Saad Bin Khalid <saadbkhalid666@gmail.com>
2026-07-06 22:14:54 +0500

    footer.js

--- src/app/page.js
+++ src/app/page.js
@@ -5,6 +5,7 @@ import { Hero } from "./sections/hero";
 import { WhyChoseUs } from "./sections/whyChooseUs";
 import { Products } from "./sections/products";
 import { GetQuote } from "./sections/getQuote";
+import { Footer } from "./sections/footer";
 
 export default function Home() {
   return (
@@ -15,6 +16,7 @@ export default function Home() {
      <WhyChoseUs   />
      <Products   />
      <GetQuote   />
+     <Footer   />
   
   </div>
   );
