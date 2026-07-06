4a34d750b25ddfb5b87cd7d0a7d9a59dbb09eca1
Saad Bin Khalid <saadbkhalid666@gmail.com>
2026-07-06 22:14:14 +0500

    getQuote.js

--- src/app/sections/getQuote.js
+++ src/app/sections/getQuote.js
@@ -1,10 +1,16 @@
 export const GetQuote = () => {
 
 return (
-    <div>
-        <h1>Ready to Order Wholesale Water?</h1>
-        <p>Join the network of business that priortize quality and efficiency.</p>
-        <button>Get a Custom Quote</button>
+    <div className="bg-gray-200 px-20 py-20">
+
+    <div className="bg-red-700 py-40 rounded-4xl  ">
+        <h1 className="text-3xl md:text-5xl font-bold text-white text-center">Ready to Order Wholesale Water?</h1>
+        <p className="text-gray-200 text-center mt-4">Join the network of business that priortize quality and efficiency.</p>
+        <div className="flex justify-center">
+
+        <button class="bg-white text-red-500 px-4 border-2 border-black  py-5 mt-4 rounded-2xl   hover:scale-105 transition-all shadow-xl">Get a Custom Quote</button>
+        </div>
+    </div>
     </div>
 )
 
