20e76c3b049a29b150538b4f9a5d4c0031310206
Saad Bin Khalid <saadbkhalid666@gmail.com>
2026-07-07 16:32:57 +0500

    providers'

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,8 +1,8 @@
 import { Geist, Geist_Mono } from "next/font/google";
-
 import "./globals.css";
 import TargetCursor from "./components/cursor/cursor";
 import { Navbar } from "./components/navbar";
+import { Providers } from "./redux/provider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -19,25 +19,27 @@ export const metadata = {
   description: "Wholesale Water Bottles",
 };
 
-
 export default function RootLayout({ children }) {
   return (
     <html
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-
       <body className="min-h-full flex flex-col">
         <TargetCursor
-        spinDuration={2}
-        hideDefaultCursor
-        parallaxOn
-        hoverDuration={0.2}
-        cursorColor="#ffffff"
-        cursorColorOnTarget="#B497CF"
-      />
-      <Navbar   />
-        {children}</body>
+          spinDuration={2}
+          hideDefaultCursor
+          parallaxOn
+          hoverDuration={0.2}
+          cursorColor="#ffffff"
+          cursorColorOnTarget="#B497CF"
+        />
+        <Providers>
+
+        <Navbar />
+        {children}
+        </Providers>
+      </body>
     </html>
   );
 }
--- src/app/redux/slice.js
+++ src/app/redux/slice.js
@@ -8,7 +8,7 @@ const cartSlice = createSlice({
   name: "cart",
   initialState,
 
-  reducer: {
+  reducers: {
     addToCart: (state, action) => {
       const item = state.cartItems.find(
         (product) => product.id === action.payload.id,
@@ -19,13 +19,36 @@ const cartSlice = createSlice({
         state.cartItems.push({ ...action.payload, quantity: 1 });
       }
     },
-    removeFromCart:(state,action)=>{
-        state.cartItems = state.cartItems.filter(item=>item.id !== action.payload)
+    removeFromCart: (state, action) => {
+      state.cartItems = state.cartItems.filter(
+        (item) => item.id !== action.payload,
+      );
     },
-    increaseQuantity:(state,action)=>{
-        const item = state.cartItems.find( product => product.id === action.payload.id)
+    increaseQuantity: (state, action) => {
+      const item = state.cartItems.find(
+        (product) => product.id === action.payload,
+      );
+      if (item) {
+        item.quantity += 1;
+      }
+    },
+    decreaseQuantity: (state, action) => {
+      const item = state.cartItems.find(
+        (product) => product.id === action.payload,
+      );
+      if (item && item.quantity > 1) {
+        item.quantity -= 1;
+      } else {
+        state.cartItems = state.cartItems.filter(
+          (product) => product.id !== action.payload,
+        );
+      }
+    },
+    clearCart:(state)=>{
+        state.cartItems=[]
     }
   },
 });
 
+export const {addToCart,removeFromCart,increaseQuantity,decreaseQuantity,clearCart} = cartSlice.actions
 export default cartSlice.reducer;
