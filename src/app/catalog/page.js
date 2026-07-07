e399207517b205cea018189c142ddbd3b160f505
Saad Bin Khalid <saadbkhalid666@gmail.com>
2026-07-07 14:22:38 +0500

    pgae.js

--- src/app/catalog/page.js
+++ src/app/catalog/page.js
@@ -2,32 +2,33 @@ export default function Page(){
 
 // data/products.js
 
-export const products = [
+ const products = [
   {
     id: 1,
     slug: "nestle-water-500ml",
     name: "Nestle Water 500ml",
-    price: 50,
+    price: 700,
     image: "/500ml.png",
   },
   {
     id: 2,
     slug: "nestle-water-1-5l",
     name: "Nestle Water 1.5L",
-    price: 120,
+    price: 560,
     image: "/1.5l.png",
   },
   {
     id: 3,
     slug: "nestle-water-5l",
     name: "Nestle Water 5L",
-    price: 250,
+    price: 350,
     image: "/5l.png",
   },
 ];
 
-  return <div>
+  return <div className="mx-auto bg-gray-100 w-full h-max">
 
+<p>Store / All Products</p>
 <h1>Wholesale Catalog</h1>
 <p>Premium hydration solutions at industrial scale. specialized pricing for retail chains, distributions, and logistics partners.</p>
 
