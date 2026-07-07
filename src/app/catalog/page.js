9273efd6c48dc508934c1ef38e39bb07cfa7e79f
Saad Bin Khalid <saadbkhalid666@gmail.com>
2026-07-07 14:16:10 +0500

    page.js of catalog

--- src/app/catalog/page.js
+++ src/app/catalog/page.js
@@ -1,3 +1,35 @@
 export default function Page(){
-  return <h1>Hello </h1>;
+
+// data/products.js
+
+export const products = [
+  {
+    id: 1,
+    slug: "nestle-water-500ml",
+    name: "Nestle Water 500ml",
+    price: 50,
+    image: "/500ml.png",
+  },
+  {
+    id: 2,
+    slug: "nestle-water-1-5l",
+    name: "Nestle Water 1.5L",
+    price: 120,
+    image: "/1.5l.png",
+  },
+  {
+    id: 3,
+    slug: "nestle-water-5l",
+    name: "Nestle Water 5L",
+    price: 250,
+    image: "/5l.png",
+  },
+];
+
+  return <div>
+
+<h1>Wholesale Catalog</h1>
+<p>Premium hydration solutions at industrial scale. specialized pricing for retail chains, distributions, and logistics partners.</p>
+
+  </div>
 };
