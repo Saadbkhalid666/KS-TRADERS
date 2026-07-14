a0bc2554a6b2a49e70b37ee5600cb408b0082370
Saad Bin Khalid <saadbkhalid666@gmail.com>
2026-07-14 14:13:36 +0500

    adding loader

---
+++ src/app/loading.js
@@ -0,0 +1,10 @@
+export default function Loader(){
+
+    
+ return <div className="relative h-screen w-full flex justify-center ">
+
+ <div className="loader absolute top-[50%] left-[50%] translate-[50% 50%] " />
+ </div>   
+    
+}
+
