4df867a66aa8cfe8cdb244773786875cf894ae9a
Saad Bin Khalid <saadbkhalid666@gmail.com>
2026-07-14 12:26:23 +0500

    whychooseus.js

--- src/app/sections/whyChooseUs.js
+++ src/app/sections/whyChooseUs.js
@@ -1,8 +1,35 @@
-"use client"
+"use client";
 import { Banknote, ShieldCheck, Store, Truck } from "lucide-react";
 import { melfira, sansation } from "../fonts/fonts";
+import { useEffect, useRef } from "react";
+import { usePathname } from "next/navigation";
+import gsap from "gsap";
 
 export const WhyChoseUs = () => {
+  const cardRef = useRef(null);
+  const pathname = usePathname();
+
+  useEffect(() => {
+    gsap.fromTo(
+      cardRef.current,
+      {
+        y: 100,
+        opacity: 1,
+        duration: 1,
+        scrollTrigger: {
+          trigger: cardRef.current,
+          start: "top 80%",
+          end: "bottom 20%",
+          scrub: true,
+        },
+      },
+      {
+        y: 0,
+        opacity: 1,
+      },
+    );
+  }, [pathname]);
+
   const featuresCards = [
     {
       id: "1",
@@ -36,18 +63,20 @@ export const WhyChoseUs = () => {
 
   return (
     <div className=" pt-20 pb-10 bg-[#f8f8fa] mx-auto ">
-        
-
       <div className="grid grid-cols-1  sm:grid-cols-2 lg:grid-cols-4 gap-4 ml-2 mr-2 ">
         {featuresCards.map((item, index) => {
-            return (
-                <div key={index} className="bg-white px-10 py-15 rounded-3xl border-2 border-gray-400 cursor-target hover:scale-105 transition-all duration-300 ease-in-out hover:shadow-2xl ">
+          return (
+            <div
+            ref={cardRef}
+              key={index}
+              className="bg-white px-10 py-15 rounded-3xl border-2 border-gray-400 cursor-target hover:scale-105 transition-all duration-300 ease-in-out hover:shadow-2xl "
+            >
               <p className="text-[#9e1e20]  bg-red-200 w-max px-4 py-3 cursor-target  rounded-2xl  ">
                 {item.icon}
               </p>
               <h1
                 className={`text-[#111111]  text-2xl font-bold ${melfira.className} pt-1 `}
-                >
+              >
                 {item.heading}
               </h1>
               <p className={`${sansation.className}`}>{item.about}</p>
